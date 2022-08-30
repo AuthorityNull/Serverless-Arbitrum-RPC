2aba3482a5ba2581651937b93ac76cf641639e82
Mike Zupper <me@mikezupper.com>
2022-08-30 14:37:15 -0400

    moved away from KV list to read operation

--- cloudflare-rpc-worker/worker_code_with_kv.js
+++ cloudflare-rpc-worker/worker_code_with_kv.js
@@ -5,7 +5,7 @@
  *
  */
 
-const pruneRPCFailedNodes = (logger, rpc_array, bad_rpc_array) => {
+ const pruneRPCFailedNodes = (logger, rpc_array, bad_rpc_array) => {
   logger(`[pruneRPCFailedNodes] - input =  ${rpc_array}, ${bad_rpc_array}`);
   let map = new Map();
 
@@ -14,7 +14,7 @@ const pruneRPCFailedNodes = (logger, rpc_array, bad_rpc_array) => {
   if (bad_rpc_array) {
     bad_rpc_array.forEach((br) => {
       rpc_array.forEach((rpc) => {
-        if (rpc == br.name) {
+        if (rpc == br) {
           map.delete(rpc);
         }
       });
@@ -31,23 +31,42 @@ const fetchRPCUrlWinner = async (env, logger) => {
   logger(`[fetchRPCUrlWinner] ARB_RPC_LIST = ${env.ARB_RPC_LIST}`);
 
   //parse the list of configured RPC enpoints (https://rpc1,https://rpc2,etc..)
-  const bad_rpc_array = await env.RPC_ERROR_LIST.list();
+  let bad_rpc_array = await env.RPC_ERROR_LIST.get("data");
+  if(!bad_rpc_array)
+    bad_rpc_array = {failed_urls:[]};
+  else
+    bad_rpc_array=JSON.parse(bad_rpc_array)
+
   logger(
     `[fetchRPCUrlWinner] RPC_ERROR_LIST = ${JSON.stringify(bad_rpc_array)}`
   );
   const rpc_urls = pruneRPCFailedNodes(
     logger,
     env.ARB_RPC_LIST.split(","),
-    bad_rpc_array.keys
+    bad_rpc_array.failed_urls
   );
 
   // choose a random winner to "balance" the load across many RPC providers
   const winner = rpc_urls[Math.floor(Math.random() * rpc_urls.length)];
   logger(`[fetchRPCUrlWinner] winner = ${winner}`);
-  return winner;
+  return {bad_rpc_array: bad_rpc_array,winner};
 };
 
-const buildResponse = async (env, logger, winner, rawResponse) => {
+const buildResponse = async (env, logger, winner,failed_urls, rawResponse) => {
+  let status= await rawResponse.status
+  const fatalErrorCodes = env.FAILURE_ERROR_CODE_LIST;
+  const errorCodes=fatalErrorCodes.split(",");
+  if(errorCodes.includes(`${status}`)){
+    console.log("sadfdaf",failed_urls)
+    await env.RPC_ERROR_LIST.put("data",JSON.stringify({failed_urls: [...failed_urls,winner]}));
+
+    let msg=`{"jsonrpc":"2.0","id":67,"error": {"code": -9911, "message": "failed status check ${status}"} }`;
+    return new Response(msg,
+    {
+      status: 500,headers: {
+            "Content-Type": "application/json",
+    }});
+  }
   let respBody = await rawResponse.json();
 
   //construct final response back to consumer
@@ -61,9 +80,8 @@ const buildResponse = async (env, logger, winner, rawResponse) => {
     if (code) {
       const msg = `Internal Server Error error code [${code}] and message [${message}]`;
       logger(`[buildResponse] error code found in response`, msg);
-      const fatalErrorCodes = env.FAILURE_ERROR_CODE_LIST;
 
-      fatalErrorCodes.split(",").forEach((fatalErrorCode) => {
+      errorCodes.forEach((fatalErrorCode) => {
         const fc = parseInt(fatalErrorCode);
         logger(`${code == fc} ${code} == ${fc} ???`);
         if (code == fc) {
@@ -73,7 +91,7 @@ const buildResponse = async (env, logger, winner, rawResponse) => {
       logger(`[buildResponse] has error = ${hasError}`);
 
       if (hasError == true) {
-        await env.RPC_ERROR_LIST.put(winner, Date.now());
+        await env.RPC_ERROR_LIST.put("data",JSON.stringify({failed_urls: [winner]}));
         return new Response(msg, { status: 500 });
       }
     }
@@ -106,7 +124,7 @@ export default {
       return new Response("Bad Request: No Input", { status: 400 });
     }
 
-    let winner = await fetchRPCUrlWinner(env, logger);
+    let {bad_rpc_array,winner} = await fetchRPCUrlWinner(env, logger);
 
     if (!winner) {
       winner = env.FALLBACK_ARB_RPC_URL;
@@ -123,12 +141,12 @@ export default {
         },
         body,
       });
-      return buildResponse(env, logger, winner, rawResponse);
+      return buildResponse(env, logger, winner, bad_rpc_array.failed_urls,rawResponse);
     } catch (e) {
       const msg = "Internal Server Error - [fetch] fetching response";
       console.error(msg, JSON.stringify(e));
       if (winner != env.FALLBACK_ARB_RPC_URL) {
-        await env.RPC_ERROR_LIST.put(winner, Date.now());
+        await env.RPC_ERROR_LIST.put("data",JSON.stringify({failed_urls: [...failed_rpcs,winner]}));
         return new Response(msg, { status: 500 });
       }
       else {
